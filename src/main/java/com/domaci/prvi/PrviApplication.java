cb64d6fb22e8bb56aa32dd57f0d61f815eef1613
zeljkobar <zeljkobar@gmail.com>
2020-07-06 01:02:29 +0200

    uradjen prvi domaci

---
+++ src/main/java/com/domaci/prvi/PrviApplication.java
@@ -0,0 +1,13 @@
+package com.domaci.prvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domaci/prvi/PrviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domaci.prvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
